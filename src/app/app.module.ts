8835cd07305b79639ace0f1870a3f48db1cd088f
Christian Morrow <morrowchristian@icloud.com>
2024-12-17 02:52:02 +0000

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ClientFormComponent } from './client-form/client-form.component';
@@ -9,16 +11,15 @@ import { ClientListComponent } from './client-list/client-list.component';
 import { ClientService } from './services/client.service';
 import { AppRoutingModule } from './app-routing.module';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    ClientFormComponent,
-    ClientListComponent
-  ],
+@NgModule({    
   imports: [
+    AppComponent,
     BrowserModule,
     FormsModule,
+    ClientFormComponent,
+    ClientListComponent,
     HttpClientModule,
+    RouterModule,
     AppRoutingModule
   ],
   providers: [ClientService],
